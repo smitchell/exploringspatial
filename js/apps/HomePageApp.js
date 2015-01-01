ee5586400f8829414d57aca1be09d226f1abca78
Steve Mitchell <stevecmitchell@gmail.com>
2014-12-31 19:22:04 -0600

    Adjusted MapBox and ArcGIS Online widths.

--- js/apps/HomePageApp.js
+++ js/apps/HomePageApp.js
@@ -6,7 +6,7 @@ define([
 ], function($, _, Backbone, Router){
   var initialize = function(){
     // Pass in our Router module and call it's initialize function
-    Router.initialize({mapWidth: 600, mapHeight: 450});
+    Router.initialize({mapWidth: '100%', mapHeight: 450});
   };
 
   return {
