e17bd0cb6efce6170583e0b4b6068ea9581a44a8
Steve Mitchell <stevecmitchell@gmail.com>
2015-12-08 21:43:47 -0600

    Made router paths lazy loaded with nested require[] blocks. Updated homepage.

--- js/router.js
+++ js/router.js
@@ -4,79 +4,98 @@ define([
     'backbone',
     'views/MenuView',
     'views/FooterView',
-    'views/home/HomePageView',
-    'views/about/AboutPageView',
-    'views/LicensePageView',
-    'views/demos/DemoIndexView',
-    'views/demos/DemoPageView'
-], function ($, _, Backbone, MenuView, FooterView, HomePageView, AboutPageView, LicensePageView, DemoIndexView, DemoPageView) {
+    'views/LicensePageView'
+], function ($, _, Backbone, MenuView, FooterView, LicensePageView) {
     var Router = Backbone.Router.extend({
         routes: {
-            "demo/:demoId" : "demo",
-            "demos" : "demos",
-            "about" : "about",
-            "license" : "license",
+            "demo/:demoId": "demo",
+            "demos": "demos",
+            "about": "about",
+            "license": "license",
             "*actions": "home"
         }
     });
 
-    var initialize = function (args) {
+    var initialize = function () {
         var router = new Router(this);
         var menuView = new MenuView({el: $('#navContainer')});
         new FooterView({el: $('#footer')});
         var contentWrapper = $('#content');
         var homePageView = null;
         var aboutPageView = null;
-        var licensePageView  = null;
+        var licensePageView = null;
         var demoPageView = null;
         var demoIndexView = null;
-        router.on('route:home', function (actions) {
-            $('.overlay').hide();
+        $('.overlay').hide();
+
+        router.on('route:home', function () {
             if (homePageView == null) {
-                homePageView = new HomePageView({el: contentWrapper});
+                require(['views/home/HomePageView'], function (HomePageView) {
+                    homePageView = new HomePageView({el: contentWrapper});
+                    homePageView.render();
+                    menuView.changeMenu('home');
+                });
+            } else {
+                homePageView.render();
+                menuView.changeMenu('home');
             }
-            homePageView.render();
-            menuView.changeMenu('home')
         });
-        router.on('route:demos', function (actions) {
-            $('.overlay').hide();
+
+        router.on('route:demos', function () {
             if (demoIndexView == null) {
-                demoIndexView = new DemoIndexView({el: contentWrapper});
+                require(['views/demos/DemoIndexView'], function (DemoIndexView) {
+                    demoIndexView = new DemoIndexView({el: contentWrapper});
+                    demoIndexView.render();
+                    menuView.changeMenu('demos')
+                });
+            } else {
+                demoIndexView.render();
+                menuView.changeMenu('demos')
             }
-            demoIndexView.render();
-            menuView.changeMenu('demos')
         });
+
         router.on('route:demo', function (demoId) {
             var numericId = Number(demoId);
             if (isNaN(numericId)) {
                 throw new Error('DemoId must be numeric.');
             }
             if (demoPageView == null) {
-                demoPageView = new DemoPageView({el: contentWrapper, demoId: numericId, router: router});
+                require(['views/demos/DemoPageView'], function (DemoPageView) {
+                    demoPageView = new DemoPageView({el: contentWrapper, demoId: numericId, router: router});
+                    demoPageView.render(numericId);
+                    menuView.changeMenu('')
+                });
             } else {
                 demoPageView.render(numericId);
+                menuView.changeMenu('')
             }
-            menuView.changeMenu('')
         });
-        router.on('route:about', function (actions) {
-            $('.overlay').hide();
+
+        router.on('route:about', function () {
             if (aboutPageView == null) {
-                aboutPageView = new AboutPageView({el: contentWrapper});
+                require(['views/about/AboutPageView'], function (AboutPageView) {
+                    aboutPageView = new AboutPageView({el: contentWrapper});
+                    aboutPageView.render();
+                    menuView.changeMenu('about')
+                });
+            } else {
+                aboutPageView.render();
+                menuView.changeMenu('about')
             }
-            aboutPageView.render();
-            menuView.changeMenu('about')
         });
-        router.on('route:license', function (actions) {
-            $('.overlay').hide();
+
+        router.on('route:license', function () {
             if (licensePageView == null) {
                 licensePageView = new LicensePageView({el: contentWrapper});
             }
             licensePageView.render();
-        });
-        Backbone.history.start();
-      };
 
-      return {
+        });
+
+        Backbone.history.start();
+    };
+
+    return {
         initialize: initialize
-      };
-    });
+    };
+});
